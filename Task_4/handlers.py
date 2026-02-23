a569eb3578770dfab9d572eafb79d1ce8c40644c
Borys Zhyhalo <snach@ukr.net>
2026-02-23 16:45:15 +0200

    Added final version task_4

--- Task_4/handlers.py
+++ Task_4/handlers.py
@@ -1,44 +1,28 @@
 from typing import List, Dict
+from decorators import input_error
 
-
+@input_error
 def add_contact(args: List[str], contacts: Dict[str, str]) -> str:
-    if len(args) != 2:
-        return "Please provide name and phone"
-
     name, phone = args
     contacts[name] = phone
     return "Contact added."
 
-
+@input_error
 def change_contact(args: List[str], contacts: Dict[str, str]) -> str:
-    if len(args) != 2:
-        return "Please provide name and phone"
-
     name, phone = args
+    contacts[name]
+    contacts[name] = phone
+    return "Contact updated."
 
-    if name in contacts:
-        contacts[name] = phone
-        return "Contact updated."
-
-    return "Contact not found."
-
-
+@input_error
 def show_phone(args: List[str], contacts: Dict[str, str]) -> str:
-    if len(args) != 1:
-        return "Please provide name"
-
     name = args[0]
+    return contacts[name]
 
-    if name in contacts:
-        return contacts[name]
-
-    return "Contact not found."
-
-
+@input_error
 def show_all(args: List[str], contacts: Dict[str, str]) -> str:
     if not contacts:
         return "No contacts found."
-
     lines = [f"{name}: {phone}" for name, phone in contacts.items()]
     return "\n".join(lines)
 
