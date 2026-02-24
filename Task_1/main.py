58b0551d0b5baffb6e74e4677b7483e410b3a3b2
Borys Zhyhalo <snach@ukr.net>
2026-02-24 20:14:46 +0200

    Updated task_1

--- Task_1/main.py
+++ Task_1/main.py
@@ -7,10 +7,8 @@ def caching_fibonacci():
 
     # функція fibonacci(n) повертає число
     def fibonacci(n):
-        if n <= 0:
-            return 0
-        if n == 1:
-            return 1
+        if n < 2:
+            return n
         if n in cache: 
             return cache[n] # якщо значення вже обчислено — повертаємо з кешу
         
